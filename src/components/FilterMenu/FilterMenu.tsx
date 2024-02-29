807e884fd8bb993f89154f3dac01c03a61c30788
Paul Lightman <pavel.kulyasov@gmail.com>
2024-02-29 23:41:29 +0300

    test

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,6 +1,5 @@
 import {Flex, Input, Select} from 'antd'
-import {FC, useEffect} from 'react'
-import {productService} from '../../services/productsService.ts'
+import {FC} from 'react'
 import {IProduct} from '../../types/interfaces/product.interface.ts'
 
 interface Props {
@@ -9,16 +8,6 @@ interface Props {
 
 export const FilterMenu: FC<Props> = ({products}) => {
 	const brands = Array.from(new Set(products?.map(product => product.brand)))
-
-	const asd = async () => {
-		const res = await productService.getAllProductsByFilter(150, 'Piaget')
-		const result = await productService.getProductsByIds(res)
-		console.log(result)
-	}
-
-	useEffect(() => {
-		void asd()
-	}, [])
 
 	return (
 		<Flex gap='20px' align='center' justify='center' style={{height: '60px'}}>
